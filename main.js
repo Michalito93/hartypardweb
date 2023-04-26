6a681aae31e9a2124b89a73befd8b71685bdd32a
Michalito93 <michal.cebula93@gmail.com>
2023-04-26 23:30:46 +0200

    Customized menu button and added sidebar menur animation

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuBtn = document.querySelector(".menu-button");
 const menu = document.querySelector(".menu");
+const logoImg = document.querySelector(".logo__image");
+const logoText = document.querySelector(".logo__text");
+const homePage = document.querySelector(".home-page");
 
 const openMenu = () => {
   menu.classList.toggle("menu--active");
+  logoImg.classList.toggle("logo__image--inactive");
+  logoText.classList.toggle("logo__text--active");
+  menuBtn.classList.toggle("menu-button--active");
+  homePage.classList.toggle("page--select");
 };
 
 menuBtn.addEventListener("click", openMenu);
