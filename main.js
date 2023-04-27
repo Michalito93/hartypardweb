58fab0387ac7d35f37f1f3ef580542b8a33a79f1
Michalito93 <michal.cebula93@gmail.com>
2023-04-27 22:42:56 +0200

    Added about section in index.html and added styles for it

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ const menu = document.querySelector(".menu");
 const logoImg = document.querySelector(".logo__image");
 const logoText = document.querySelector(".logo__text");
 const homePage = document.querySelector(".home-page");
+const aboutPage = document.querySelector(".about-page");
 
 const openMenu = () => {
   menu.classList.toggle("menu--active");
   logoImg.classList.toggle("logo__image--inactive");
   logoText.classList.toggle("logo__text--active");
   menuBtn.classList.toggle("menu-button--active");
-  homePage.classList.toggle("page--select");
+  homePage.classList.toggle("home-page--select");
+  aboutPage.classList.toggle("about-page--select");
 };
 
 menuBtn.addEventListener("click", openMenu);
